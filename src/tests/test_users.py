8bcc7402013ca0ca6009e3f55c74024f3357a8f7
Ninad Bhangui <b.ninad50@gmail.com>
2023-04-14 18:53:07 +0530

    tests and routes added

---
+++ src/tests/test_users.py
@@ -0,0 +1,61 @@
+import json
+from src.api.models import User
+
+def test_add_user(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post("/users", data=json.dumps({
+        "username": "john",
+        "email": "john@seven.io"
+        }), content_type="application/json")
+
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 201
+    assert "john@seven.io was added!" in data["message"]
+
+def test_add_user_invalid_json(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post("/users", data=json.dumps({
+            "email": "john@seven.io",
+        }), content_type="application/json",)
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Input payload validation failed" in data["message"]
+
+def test_add_user_duplicate_email(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post("/users", data=json.dumps({
+        "username": "john",
+        "email": "john@seven.io"
+        }), content_type="application/json",)
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Sorry. That email already exists." in data["message"]
+
+def test_single_user(test_app, test_database, add_user):
+    user = add_user("william", "william@seven.io")
+    client = test_app.test_client()
+    resp = client.get(f"/users/{user.id}")
+    data = json.loads(resp.data.decode())
+    print(data)
+    assert resp.status_code == 200
+    assert "william" in data["username"]
+
+def test_single_user_incorrect_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.get(f"/users/999")
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert "User 999 does not exist" in data["message"]
+
+def test_all_users(test_app, test_database, add_user):
+    test_database.session.query(User).delete()
+    add_user("john", "johndoe@seven.org")
+    add_user("david", "davidmills@seven.com")
+    client = test_app.test_client()
+    resp = client.get("/users")
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert len(data) == 2
+    assert "john" in data[0]["username"]
+    assert "johndoe@seven.org" in data[0]["email"]
+    assert "david" in data[1]["username"]
