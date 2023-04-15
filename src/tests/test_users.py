9baf5f74db009dea97b4ae5169b97f4875d1bf17
Ninad Bhangui <b.ninad50@gmail.com>
2023-04-15 17:59:57 +0530

    flake8 and coverage

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -55,7 +55,7 @@ def test_single_user(test_app, test_database, add_user):
 
 def test_single_user_incorrect_id(test_app, test_database):
     client = test_app.test_client()
-    resp = client.get(f"/users/999")
+    resp = client.get("/users/999")
     data = json.loads(resp.data.decode())
     assert resp.status_code == 404
     assert "User 999 does not exist" in data["message"]
